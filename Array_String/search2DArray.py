91e83319b3251d847fc068cd6b2d982addfb05ac
JIANG Zongqing <1908932021@qq.com>
2024-06-07 23:06:16 +0800

    binary search  slove 搜索二维矩阵

--- Array_String/search2DArray.py
+++ Array_String/search2DArray.py
@@ -1,16 +1,29 @@
 import sys
+import io
+import json
 from typing import List
+# input:
+# [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+# 3
 
 def stringToIntegerList(line):
-    return list(map(int, line.split()))
+    return json.loads(line)
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # 遍历每一行
-        for row in matrix:
-            # 检查当前行是否包含目标值
-            if target in row:
+        if not matrix or not matrix[0]:
+            return False
+        
+        rows, cols = len(matrix), len(matrix[0])
+        row, col = 0, cols - 1 # cols - 1 : Sequence  retrieval from large to small
+        while row < rows and col >= 0 :
+            if target ==  matrix[row][col]:
                 return True
+            elif target < matrix[row][col]:
+                col -= 1 # retrieval in this line which is numbe "row"
+            else:
+                row += 1 # key point: similar to binary search
+
         return False
 
 def Entermain():
@@ -20,11 +33,13 @@ def Entermain():
 
     lines = readlines()
     # 读取第一行，获取矩阵的行数和列数
-    m, n = map(int, next(lines).split())
+    matrix_str = next(lines)
+    matrix = json.loads(matrix_str)  # 使用 json 直接解析整个矩阵
     # 读取下一行，获取目标值
-    target = int(next(lines))
-    # 读取剩余的行，构建矩阵
-    matrix = [stringToIntegerList(next(lines)) for _ in range(m)]
+    # 读取目标值
+    target_str = next(lines)
+    target = int(target_str)
+
     # 创建Solution对象并调用searchMatrix方法
     result = Solution().searchMatrix(matrix, target)
     # 打印结果
