143123b7f637d7fe0b750db14b2963d0822e890f
JIANG Zongqing <1908932021@qq.com>
2024-06-14 17:16:57 +0800

    add analyze for Diffusion

--- Diffusion/simpleDiffusionModel.py
+++ Diffusion/simpleDiffusionModel.py
@@ -4,31 +4,51 @@ import matplotlib.pyplot as plt
 
 
 class SimpleDiffusionModel:
+    # beta_start: Noise intensity at the beginning of the diffusion process
     def __init__(self, timesteps, beta_start, beta_end) -> None:
         self.timesteps = timesteps
-        self.betas = np.linspace(beta_start, beta_end, timesteps)
-        self.alphas = 1 - self.betas
-        self.alphas_bars = np.cumprod(self.alphas)
+        # betas : add noise, alphas : denoising
+        self.betas = np.linspace(beta_start, beta_end, timesteps) # Linear interpolated array of noise intensity, length is timesteps
+        self.alphas = 1 - self.betas                    # The aplha value of each time step is calculated to represent the denoising retention ratio
+        self.alphas_bars = np.cumprod(self.alphas)      # the cumlative product represents the total retention ratio from time step 0 to the current time step
 
-    def forward_diffsion(self, x0):
+    '''
+    ### 1. Forward Diffusion Process
+    Starting with a distribution of complex data (such as a picture), 
+    noise is gradually added so that it becomes closer and closer to a simple known distribution (such as Gaussian distribution)
+    '''
+    def forward_diffsion(self, x0): # x0: raw picture or signals
         noise = np.random.normal(size=x0.shape)
         xt = x0
         trajectory = [xt]
-        for t in range(self.timesteps):
-            xt = np.sqrt(self.alphas[t]) * xt + np.sqrt(self.betas[t]) * noise
+        for t in range(self.timesteps): 
+            xt = np.sqrt(self.alphas[t]) * xt + np.sqrt(self.betas[t]) * noise    # np.sqrt(self.betas[t]) : Intensity of noise
             trajectory.append(xt)
         return trajectory
     
+    """
+    ### 2. Reverse Diffusion Process
+    Learn a reverse process, strarting with a simple 'known distribution', gradually removing noise and returning to a distribution of complex data.
+    This process is accomplished by training a neural network that learns how to remove noise at each step
+    """
     def reverse_diffusion(self, xt):
         noise = np.random.normal(size=xt.shape)
         for t in reversed(range(self.timesteps)):
+            # xt: The result of the last time step of the forward diffusion process
+            # np.sqrt(self.betas[t]) * noise : use "-" to noise removal
+            # np.sqrt(self.alphas[t]) : use "/" to scale
             xt = (xt - np.sqrt(self.betas[t]) * noise) / np.sqrt(self.alphas[t])
         return xt
     
+    '''
+    ### 3. Conditional Generation
+    In order to achieve generation based on text input, the model not only learns how to remove noise,
+    but also learns how to guide the denosing process according to given conditions (such as text descriptions).
+    '''
     def generate(self, x0):
-        trajectory = self.forward_diffsion(x0)
+        trajectory = self.forward_diffsion(x0)    # The trajectory of all intermediate steps in the diffusion process is obtained
         xt = trajectory[-1]
-        x0_hat = self.reverse_diffusion(xt)
+        x0_hat = self.reverse_diffusion(xt)       # The denoised data x0_hat is obtained
         return x0_hat, trajectory
     
 if __name__ == "__main__":
